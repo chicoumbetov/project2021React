b6336a44b7c7ddb8d33304bc31b9642708ef1770
chicoumbetov <shyngysmaaan@gmail.com>
2021-02-26 11:12:00 +0100

    Pagination logic, infinity scroll, reset the states to zero when new search

--- API/TheMovieDataBaseAPI.js
+++ API/TheMovieDataBaseAPI.js
@@ -3,8 +3,8 @@
 const API_TOKEN = "4336d5ada30b10dc332f193721ba4a50"
 //https://www.themoviedb.org/
 
-export const getFilmsFromApiWithSearchedText = (text) => {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text;
+export const getFilmsFromApiWithSearchedText = (text, page) => {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page;
     return fetch(url)
         .then((response) => response.json())
         .then((json) => {
--- components/Search.js
+++ components/Search.js
@@ -12,11 +12,13 @@ import { getFilmsFromApiWithSearchedText } from '../API/TheMovieDataBaseAPI';
 class Search extends Component {
   constructor(props) {
     super(props)
+    this.searchedText = ""
+    this.page = 0 // Compteur pour connaître la page courante
+    this.totalPages = 0 // Nombre de pages totales pour savoir si on a atteint la fin des retours de l'API TMDB
     this.state = {
       films: [],
       isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
     }
-    this.searchedText = ""
   }
 
   _displayLoading() {
@@ -30,15 +32,19 @@ class Search extends Component {
     }
   }
 
+  //this function will be called in _searchFilms() to initialize to zero.
   _loadFilms() {
     //search movie ONLY if text is typed
     if (this.searchedText.length > 0) {
+
       //Lancement du charchement (preloader)
       this.setState({ isLoading: true });
       //search movie by typed text
-      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+      getFilmsFromApiWithSearchedText(this.searchedText, this.page + 1).then(data => {
+        this.page = data.page
+        this.totalPages = data.total_pages
         this.setState({
-          films: data.results,
+          films: [...this.state.films, ...data.results], //same as this.state.films.concat(data.results)
           isLoading: false //Arrêt du charchement stop preloader
         })
       });
@@ -50,6 +56,21 @@ class Search extends Component {
     this.searchedText = text;
   }
 
+  _searchFilms() {
+    //Ici on a remis à zero les films de notre state
+    this.page = 0;
+    this.totalPages = 0;
+    this.setState({
+      films: []
+    }, () => {
+      //It is called and return data just right after initializing of setState and putting everything in data to zero
+      this._loadFilms();
+
+      //J'utilise la paramètre length sur mon tableau de films pour vérifier qu'il y a bien 0 film
+      //console.log("Page: " + this.page + " / TotalPages: " + " / Nombre de films: " + this.state.films.length)
+    })
+
+  }
 
   render() {
     console.log(this.state.isLoading); //check if isLoading switches between false and true
@@ -61,19 +82,21 @@ class Search extends Component {
           onChangeText={(text) => this._searchTextInputChanged(text)}
 
           //let us search by clicking "enter" on keyboard and not only by clicking on "rechercher" button
-          onSubmitEditing={() => this._loadFilms()}
+          onSubmitEditing={() => this._searchFilms()}
 
           style={[styles.textinput, { marginBottom: 5 }]}
           placeholder='Titre du film' />
         <Button title='Rechercher'
-          onPress={() => this._loadFilms()}
+          onPress={() => this._searchFilms()}
         />
         <FlatList
           data={this.state.films}
           keyExtractor={item => item.id.toString()}
           onEndReachedThreshold={0.5}
-          onEndReached={( {distanceFromEnd}) => {
-            console.log("onEndReached", distanceFromEnd)
+          onEndReached={() => {
+            if (this.page < this.totalPages) { // On vérifie qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+              this._loadFilms()
+            }
           }}
           renderItem={({ item }) => <FilmItem film={item} />}
         />
